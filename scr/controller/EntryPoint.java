59e8df2ac9f31011e22a217e2ead4c6cab04d6b2
Peter Janosky <pcjanosky@gmail.com>
2021-06-20 13:50:21 -0400

    Converted README to markdown and moved main class to controller

--- scr/controller/EntryPoint.java
+++ scr/controller/EntryPoint.java
@@ -1,15 +1,20 @@
-import controller.ImageProcessingController;
-import controller.SimpleImageProcessingController;
+package controller;
+
 import java.io.InputStreamReader;
 import model.ImageProcessingModel;
 import model.ImageProcessingModelImpl;
 
 /**
- * A simple controller for the sole purpose of producing images for testing. Produces sample images
- * using ImageProcessingModelImp.
+ * An entry point for running the program
  */
-public class TestController {
+public class EntryPoint {
 
+  /**
+   * Runs the image processing program. Creates a new controller, model and view, and runs the
+   * controller.
+   *
+   * @param args the arguments for the program. Currently being ignored.
+   */
   public static void main(String[] args) {
     ImageProcessingModel model = new ImageProcessingModelImpl();
     Readable input = new InputStreamReader(System.in);
