f437929177881ef6d1e173cce60aaa998abce074
Peter Janosky <pcjanosky@gmail.com>
2021-06-19 22:45:43 -0400

    Added new test for AddCommand

---
+++ test/AddCommandTest.java
@@ -0,0 +1,68 @@
+import static org.junit.Assert.assertEquals;
+
+import controller.commands.AddCommand;
+import model.ImageProcessingModel;
+import model.ImageProcessingModelImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests the AddCommand class.
+ */
+public class AddCommandTest {
+
+  private ImageProcessingModel model;
+
+  /**
+   * Construct a new AddCommandTest initializing all example test data.
+   */
+  public AddCommandTest() {
+    model = new ImageProcessingModelImpl();
+  }
+
+  @Before
+  public void setup() {
+    model = new ImageProcessingModelImpl();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorNullName() {
+    new AddCommand(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorInvalidNameWhitespace() {
+    new AddCommand("hello world");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorInvalidNameSpecialChar() {
+    new AddCommand("#$%^&*");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorInvalidNameEmpty() {
+    new AddCommand("");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testGoRepeatedName() {
+    model.addLayer("layer1");
+    model.addLayer("layer2");
+    model.addLayer("layer3");
+
+    new AddCommand("layer2").go(model);
+  }
+
+  @Test
+  public void testGo() {
+    assertEquals(0, model.numLayers());
+    new AddCommand("layer1").go(model);
+    assertEquals(1, model.numLayers());
+    assertEquals("layer1", model.getLayerNameAt(0));
+
+    new AddCommand("layer2").go(model);
+    assertEquals(2, model.numLayers());
+    assertEquals("layer2", model.getLayerNameAt(1));
+  }
+}
