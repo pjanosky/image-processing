2b96044303c2cbe1dc46b13c7cfb6c7869a13ee5
Angela Rhee <angelarhee02@gmail.com>
2021-06-23 02:13:50 +0900

    added GUI controller and view

---
+++ scr/view/GUIImageProcessingView.java
@@ -0,0 +1,8 @@
+package view;
+
+import java.awt.event.ActionListener;
+
+public interface GUIImageProcessingView extends ImageProcessingView {
+  void addActionListener(ActionListener listener);
+
+}
---
+++ scr/view/GUIView.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class GUIView implements GUIImageProcessingView {
+
+  @Override
+  public void renderLayers() throws IOException {
+
+  }
+
+  @Override
+  public void renderMessage(String message) throws IOException {
+
+  }
+
+  @Override
+  public void addActionListener(ActionListener listener) {
+
+  }
+}
