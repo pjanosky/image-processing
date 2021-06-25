80baefd1d2325374ef497e16439cffa5c61b1540
Peter Janosky <pcjanosky@gmail.com>
2021-06-24 22:28:25 -0400

    Updated program entry point to support scripts, text, or gui views

--- scr/controller/EntryPoint.java
+++ scr/controller/EntryPoint.java
@@ -1,8 +1,14 @@
 package controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import model.ImageProcessingModel;
 import model.ImageProcessingModelImpl;
+import view.GUIImageProcessingView;
+import view.GUIView;
+import view.ImageProcessingTextView;
 
 /**
  * An entry point for running the program
@@ -17,10 +23,37 @@ public class EntryPoint {
    */
   public static void main(String[] args) {
     ImageProcessingModel model = new ImageProcessingModelImpl();
-    Readable input = new InputStreamReader(System.in);
-    Appendable output = System.out;
-    ImageProcessingController controller =
-        new SimpleImageProcessingController(model, input, output);
-    controller.run();
+    ImageProcessingController controller;
+
+    if (args.length == 0) {
+      GUIImageProcessingView view = new GUIView("Image Processing");
+      controller = new GUIController(model, view);
+      controller.run();
+    } else switch (args[0]) {
+      case "-script":
+        if (args.length < 2) {
+          System.out.println("No path of script file.");
+        }
+        try {
+          controller = new SimpleImageProcessingController(model,
+              new FileReader(args[1]), System.out);
+          controller.run();
+        } catch (FileNotFoundException e) {
+          System.out.println("Fail to read script file. " + e.getMessage());
+        }
+        break;
+      case "-text":
+        controller = new SimpleImageProcessingController(model,
+            new InputStreamReader(System.in), System.out);
+        controller.run();
+        break;
+      case "-interactive":
+        GUIImageProcessingView view = new GUIView("Image Processing");
+        controller = new GUIController(model, view);
+        controller.run();
+        break;
+      default:
+        System.out.println("Invalid command-line arguments.");
+    }
   }
 }
