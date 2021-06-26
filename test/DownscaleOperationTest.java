cb449675af2bce489693b2b2a177d841f2697d6f
Peter Janosky <pcjanosky@gmail.com>
2021-06-26 15:49:57 -0400

    Modified tests for DownscaleOperation to test non equal aspect ratios

--- test/DownscaleOperationTest.java
+++ test/DownscaleOperationTest.java
@@ -68,13 +68,19 @@ public class DownscaleOperationTest {
         new RgbPixel(190, 255, 87));
     Image output = new DownscaleOperation(0.25, 0.5).apply(image);
     assertEquals(3, output.getWidth());
-    assertEquals(2, output.getHeight());
+    assertEquals(4, output.getHeight());
 
     assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(0, 0));
-    assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(1, 0));
+    assertEquals(new RgbPixel(190, 255, 87), output.getPixelAt(1, 0));
+    assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(2, 0));
+    assertEquals(new RgbPixel(190, 255, 87), output.getPixelAt(3, 0));
     assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(0, 1));
-    assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(1, 1));
+    assertEquals(new RgbPixel(190, 255, 87), output.getPixelAt(1, 1));
+    assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(2, 1));
+    assertEquals(new RgbPixel(190, 255, 87), output.getPixelAt(3, 1));
     assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(0, 2));
-    assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(1, 2));
+    assertEquals(new RgbPixel(190, 255, 87), output.getPixelAt(1, 2));
+    assertEquals(new RgbPixel(3, 38, 199), output.getPixelAt(2, 2));
+    assertEquals(new RgbPixel(190, 255, 87), output.getPixelAt(3, 2));
   }
 }
